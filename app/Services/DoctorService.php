efa7f1221141059a51f86802d0a817b4812ec2a3
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2025-03-10 04:23:03 +0200

    remove repositories

--- app/Services/DoctorService.php
+++ app/Services/DoctorService.php
@@ -2,10 +2,8 @@
 
 namespace App\Services;
 
-use App\Repositories\DoctorRepository;
 class DoctorService
 {
     public function __construct(
-        private DoctorRepository $doctorRepository
     ) {}
 }
--- app/Services/SpecializationService.php
+++ app/Services/SpecializationService.php
@@ -2,10 +2,8 @@
 
 namespace App\Services;
 
-use App\Repositories\SpecializationRepository;
 class SpecializationService
 {
     public function __construct(
-        private SpecializationRepository $specializationRepository
     ) {}
 }
