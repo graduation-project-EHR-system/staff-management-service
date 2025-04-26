6fede02147074b2982b5790a95ead1927ab487a4
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2025-04-26 23:26:21 +0300

    feat: Add scramble docs

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'api'),
+        'guard' => env('AUTH_GUARD', 'web'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -40,6 +40,10 @@ return [
             'driver' => 'jwt',
             'provider' => 'users',
         ],
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users'
+        ]
     ],
 
     /*
