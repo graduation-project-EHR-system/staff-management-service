ef5b2ab88998d8cb34e433cdff82f61ceb20ce7d
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2025-05-08 14:17:20 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test' , function(Request $request) {
+    $clientIp = $request->ip();
+    \Log::info('Client IP: ' . $clientIp);
+
+    return "Client IP logged: " . $clientIp;
+ });
