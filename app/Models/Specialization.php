0ee9fd934f753d80adb1f932b196921be2e0ef0b
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2025-06-08 19:52:14 +0300

    feat: Add soft deletes to specilizations

--- app/Models/Specialization.php
+++ app/Models/Specialization.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Specialization extends Model
 {
     /** @use HasFactory<\Database\Factories\SpecializationFactory> */
-    use HasFactory, HasUuids;
+    use HasFactory, HasUuids, SoftDeletes;
 
     protected $fillable = [
         'name',
