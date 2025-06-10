72f40e4cbffb30b1b24c047e5bb62ebac21ba009
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2025-06-10 03:04:38 +0300

    feat: Add custom filters for doctor, nurse and receptionist endpoints

---
+++ app/ModelFilters/DoctorFilter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\ModelFilters;
+
+use EloquentFilter\ModelFilter;
+
+class DoctorFilter extends ModelFilter
+{
+    /**
+    * Related Models that have ModelFilters as well as the method on the ModelFilter
+    * As [relationMethod => [input_key1, input_key2]].
+    *
+    * @var array
+    */
+    public $relations = [];
+
+    public function search(string $search)
+    {
+        return $this->whereAny([
+            'first_name',
+            'last_name',
+            'email'
+        ], 'like', '%' . $search . '%');
+    }
+}
