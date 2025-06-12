ce134baa796d4e2d372429035efb94085267b897
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2025-06-12 03:00:49 +0300

    fix: Fix analytics test, the number of specializations

--- tests/Feature/Api/AnalyticsTest.php
+++ tests/Feature/Api/AnalyticsTest.php
@@ -7,15 +7,12 @@ use App\Models\Specialization;
 use Symfony\Component\HttpFoundation\Response;
 
 beforeEach(function () {
-    $this->specialization = Specialization::factory()->create();
-
     $this->withoutMiddleware();
 });
 
 test('can get staff analytics', function () {
-    Doctor::factory()->count(3)->create([
-        'specialization_id' => $this->specialization->id,
-    ]);
+    Doctor::factory()->count(3)->create();
+
     Nurse::factory()->count(2)->create();
     Receptionist::factory()->count(1)->create();
 
@@ -35,7 +32,7 @@ test('can get staff analytics', function () {
         'data' => [
             'num_of_doctors'         => 3,
             'num_of_nurses'          => 2,
-            'num_of_specializations' => 1,
+            'num_of_specializations' => 3,
             'num_of_receptionists'   => 1,
         ],
     ]);
